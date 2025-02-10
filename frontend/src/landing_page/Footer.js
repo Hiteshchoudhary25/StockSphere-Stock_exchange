6ceb9f49b13abf7f53813ff61b0af5959a7ae8f8
Hitesh Choudhary <hiteshchoudhary2508@gmail.com>
2025-02-10 08:34:44 +0530

    Completed the Home section-frontend

---
+++ frontend/src/landing_page/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+    return ( 
+       
+        <div className="container-fluid bg-white text-black py-3">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <p className="text-center">Copyright &copy; StockSphere 2022. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Footer;
--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -4,15 +4,21 @@ import Hero from './Hero';
 import Education from './Educaion';
 import Stats from './Stats';
 import Pricing from './Pricing';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import OpenAccount from '../OpenAccount';
 
 function Homepage() {
     return ( 
         <>
+        <Navbar/>
         <Hero/>
         <Awards/>
         <Stats/>
         <Pricing/>
         <Education/>
+        <OpenAccount/>
+        <Footer/>
         </>
      );
 }
