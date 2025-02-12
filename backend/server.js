dfc2da3b95dd8a109257ffc3ef254230cb837ce9
Hitesh Choudhary <hiteshchoudhary2508@gmail.com>
2025-02-12 16:44:49 +0530

    Setup connection with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
-const express = require("express");
+import express from "express";
+
+import dotenv from "dotenv";
+dotenv.config(); 
+
+const port = process.env.PORT || 5000;
+const dbUrl = process.env.MONGO_URL;
 
 const app = express();
 
-app.listen(5000 ,() => {
-    console.log("Server is running on port 5000");
+import connectToMongoose from "./db/connectToMongoose.js";
+
+connectToMongoose(dbUrl);
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, world! hitesh");
 });
+
+
+
+app.listen(port ,() => {
+    connectToMongoose();
+    console.log(`Server is running on port ${port}`)
+  });
